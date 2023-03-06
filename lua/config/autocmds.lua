0b2e2a086ff2a6ce0fd38befef90bb3871b6e9e2
Shahadul Haider <shahadul.haider@gmail.com>
2023-03-06 14:17:42 +0600

    autocommands for markdown

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -94,3 +94,34 @@ vim.cmd([[
    autocmd BufEnter .env lua vim.diagnostic.disable(0)
   augroup end
 ]])
+
+vim.api.nvim_create_augroup("_editing", { clear = true })
+
+-- Enable spell check and word wrap for certain file types
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  group = "_editing",
+  pattern = { "*.txt", "*.md", "*.tex" },
+  callback = function()
+    vim.cmd("setlocal spell")
+    vim.cmd("setlocal wrap")
+  end,
+})
+
+-- Prevent IndentLine from hiding ``` in markdown files
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  group = "_editing",
+  pattern = { "*.md", "*.markdown" },
+  callback = function()
+    vim.g["indentLine_enabled"] = 0
+    vim.g["markdown_syntax_conceal"] = 0
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = "_editing",
+  pattern = "markdown",
+  callback = function()
+    vim.g["indentLine_enabled"] = 0
+    vim.g["markdown_syntax_conceal"] = 0
+  end,
+})
