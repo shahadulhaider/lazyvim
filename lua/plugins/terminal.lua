d58855017101440edf83f844e2d8bcb05cc9058b
Shahadul Haider <shahadul.haider@gmail.com>
2023-04-07 14:27:26 +0600

    terminal

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -27,4 +27,30 @@ return {
       },
     },
   },
+
+  {
+    "willothy/flatten.nvim",
+    opts = {},
+    lazy = false,
+    priority = 1001,
+  },
+  {
+    "stevearc/overseer.nvim",
+    cmd = {
+      "OverseerOpen",
+      "OverseerClose",
+      "OverseerToggle",
+      "OverseerSaveBundle",
+      "OverseerLoadBundle",
+      "OverseerDeleteBundle",
+      "OverseerRunCmd",
+      "OverseerRun",
+      "OverseerInfo",
+      "OverseerBuild",
+      "OverseerQuickAction",
+      "OverseerTaskAction ",
+      "OverseerClearCache",
+    },
+    opts = {},
+  },
 }
