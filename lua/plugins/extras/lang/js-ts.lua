0bd3d335299fc7468ef6bb16fe20261b6524b575
Shahadul Haider <shahadul.haider@gmail.com>
2023-06-20 09:30:27 +0600

    added extra lang features

--- lua/plugins/extras/lang/js-ts.lua
+++ lua/plugins/extras/lang/js-ts.lua
@@ -80,4 +80,10 @@ return {
   --     end
   --   end,
   -- },
+
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.tailwind" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.formatting.prettier" },
+  { import = "lazyvim.plugins.extras.linting.eslint" },
 }
