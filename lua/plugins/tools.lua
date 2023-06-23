5d0a019f96cb41febc916036ea947f3b9483dbad
Shahadul Haider <shahadul.haider@gmail.com>
2023-06-23 12:33:18 +0600

    removed oil

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -1,20 +1,4 @@
 return {
-  -- file explorers
-  {
-    "stevearc/oil.nvim",
-    opts = {},
-    enabled = true,
-    cmd = "Oil",
-    keys = {
-      {
-        "<leader>o-",
-        function()
-          require("oil").open()
-        end,
-        desc = "Open folder in Oil",
-      },
-    },
-  },
 
   -- projects
   {
