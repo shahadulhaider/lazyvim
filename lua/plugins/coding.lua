419fb69ae9db3c2aa749b296f04c9755e52f315a
Shahadul Haider <shahadul.haider@gmail.com>
2023-10-14 11:21:15 +0600

    removed unnecessary plugins, refactoring keymap update

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,11 +1,5 @@
 return {
   {
-    "tiagovla/scope.nvim",
-    event = "BufRead",
-    config = true,
-  },
-
-  {
     "danymat/neogen",
     keys = {
       {
@@ -34,14 +28,15 @@ return {
     end,
     keys = {
       {
-        "<leader>r",
+        "<leader>cR",
         function()
           require("refactoring").select_refactor()
         end,
-        mode = "v",
+        mode = { "n", "x", "v" },
         noremap = true,
         silent = true,
         expr = false,
+        desc = "Select Refactor",
       },
     },
     opts = {},
@@ -75,43 +70,6 @@ return {
     end,
   },
   {
-    "simrat39/symbols-outline.nvim",
-    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
-    config = function()
-      local icons = require("lazyvim.config").icons
-      require("symbols-outline").setup({
-        symbols = {
-          File = { icon = icons.kinds.File, hl = "TSURI" },
-          Module = { icon = icons.kinds.Module, hl = "TSNamespace" },
-          Namespace = { icon = icons.kinds.Namespace, hl = "TSNamespace" },
-          Package = { icon = icons.kinds.Package, hl = "TSNamespace" },
-          Class = { icon = icons.kinds.Class, hl = "TSType" },
-          Method = { icon = icons.kinds.Method, hl = "TSMethod" },
-          Property = { icon = icons.kinds.Property, hl = "TSMethod" },
-          Field = { icon = icons.kinds.Field, hl = "TSField" },
-          Constructor = { icon = icons.kinds.Constructor, hl = "TSConstructor" },
-          Enum = { icon = icons.kinds.Enum, hl = "TSType" },
-          Interface = { icon = icons.kinds.Interface, hl = "TSType" },
-          Function = { icon = icons.kinds.Function, hl = "TSFunction" },
-          Variable = { icon = icons.kinds.Variable, hl = "TSConstant" },
-          Constant = { icon = icons.kinds.Constant, hl = "TSConstant" },
-          String = { icon = icons.kinds.String, hl = "TSString" },
-          Number = { icon = icons.kinds.Number, hl = "TSNumber" },
-          Boolean = { icon = icons.kinds.Boolean, hl = "TSBoolean" },
-          Array = { icon = icons.kinds.Array, hl = "TSConstant" },
-          Object = { icon = icons.kinds.Object, hl = "TSType" },
-          Key = { icon = icons.kinds.Key, hl = "TSType" },
-          Null = { icon = icons.kinds.Null, hl = "TSType" },
-          EnumMember = { icon = icons.kinds.EnumMember, hl = "TSField" },
-          Struct = { icon = icons.kinds.Struct, hl = "TSType" },
-          Event = { icon = icons.kinds.Event, hl = "TSType" },
-          Operator = { icon = icons.kinds.Operator, hl = "TSOperator" },
-          TypeParameter = { icon = icons.kinds.TypeParameter, hl = "TSParameter" },
-        },
-      })
-    end,
-  },
-  {
     "mbbill/undotree",
     cmd = "UndotreeToggle",
     keys = { { "<leader>uu", "<cmd>UndotreeToggle<cr>", desc = "UndoTree Toggle" } },
@@ -121,21 +79,4 @@ return {
     end,
   },
   { "mattn/emmet-vim", event = "VeryLazy" },
-  {
-    "eandrju/cellular-automaton.nvim",
-
-    keys = {
-      {
-        "<leader>zm",
-        "<cmd>CellularAutomaton make_it_rain<cr>",
-        desc = "Make it rain...",
-      },
-
-      {
-        "<leader>zg",
-        "<cmd>CellularAutomaton game_of_life<cr>",
-        desc = "Game of life...",
-      },
-    },
-  },
 }
